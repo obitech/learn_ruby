312fc1261fde2e7d05ab3e3003d9eff8d0ddd9d6
obitech <the.am0k@gmx.de>
2016-07-21 18:02:46 +0900

    almost finished 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,60 @@
 #write your code here
+def echo(arg)
+	return arg
+end
+
+def shout(arg)
+	arg.upcase
+end
+
+def repeat(arg, num = 2)
+	space = " "
+	output = "#{arg}"
+	if num <= 2
+		output = arg + " " + arg
+	else	
+		(1...num).each do
+			output <<  (" " + arg)
+		end
+	end	
+	return output
+end
+
+def start_of_word(word, pos)
+	string = word[0, pos]
+	return string
+end
+
+def first_word(word)
+	output = word.split(" ")
+	return output[0].to_s
+end
+
+def titleize(word)
+	# little_word = ["and", "to", "over", "at", "in", "the"]
+	array = word.split(" ")
+	array = array.map! {|x| x.capitalize}
+	output = array.join(" ")
+	return output
+end	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

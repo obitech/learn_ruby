15ea14740a2123773892f8bea0b97e34d5b19372
obitech <the.am0k@gmx.de>
2016-09-14 18:15:48 +0200

    05 solved

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,10 @@
 class Book
-# write your code here
+	def title
+		@title 
+	end	
+
+	def title=(t)
+		stop_words = %w[and the in a of an]
+		@title = t.capitalize.split.map { |s| stop_words.include?(s) ? s : s.capitalize  }.join(" ")
+	end	
 end
